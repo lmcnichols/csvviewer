848cb7d387d689578d53faa69b1848f2cf48f421
Logan McNichols <loganamcnichols@gmail.com>
2020-07-01 16:00:36 -0700

    commented out the _setField function

--- src/model.ts
+++ src/model.ts
@@ -546,62 +546,63 @@ export class DSVModel extends MutableDataModel implements IDisposable {
    * @param value the value typed at the keyboard
    */
 
-  _setField(row: number, column: number, value: string): void {
-    let nextIndex;
+  // _setField(row: number, column: number, value: string): void {
+  //   let nextIndex;
 
-    // Find the index for the first character in the field.
-    const index = this._getOffsetIndex(row, column);
+  //   // Find the index for the first character in the field.
+  //   const index = this._getOffsetIndex(row, column);
 
-    // Initialize the trim adjustments.
-    let trimRight = 0;
-    let trimLeft = 0;
+  //   // Initialize the trim adjustments.
+  //   let trimRight = 0;
+  //   let trimLeft = 0;
 
-    // Find the end of the slice (the start of the next field), and how much we
-    // should adjust to trim off a trailing field or row delimiter. First check
-    // if we are getting the last column.
-    if (column === this._columnCount! - 1) {
-      // Check if we are getting any row but the last.
-      if (row < this._rowCount! - 1) {
-        // Set the next offset to the next row, column 0.
-        nextIndex = this._getOffsetIndex(row + 1, 0);
+  //   // Find the end of the slice (the start of the next field), and how much we
+  //   // should adjust to trim off a trailing field or row delimiter. First check
+  //   // if we are getting the last column.
+  //   if (column === this._columnCount! - 1) {
+  //     // Check if we are getting any row but the last.
+  //     if (row < this._rowCount! - 1) {
+  //       // Set the next offset to the next row, column 0.
+  //       nextIndex = this._getOffsetIndex(row + 1, 0);
 
-        // Since we are not at the last row, we need to trim off the row
-        // delimiter.
-        trimRight += this._rowDelimiter.length;
-      } else {
-        // We are getting the last data item, so the slice end is the end of the
-        // data string.
-        nextIndex = this._data.length;
+  //       // Since we are not at the last row, we need to trim off the row
+  //       // delimiter.
+  //       trimRight += this._rowDelimiter.length;
+  //     } else {
+  //       // We are getting the last data item, so the slice end is the end of the
+  //       // data string.
+  //       nextIndex = this._data.length;
 
-        // The string may or may not end in a row delimiter (RFC 4180 2.2), so
-        // we explicitly check if we should trim off a row delimiter.
-        if (
-          this._data[nextIndex - 1] ===
-          this._rowDelimiter[this._rowDelimiter.length - 1]
-        ) {
-          trimRight += this._rowDelimiter.length;
-        }
-      }
-    } else {
-      // The next field starts at the next column offset.
-      nextIndex = this._getOffsetIndex(row, column + 1);
+  //       // The string may or may not end in a row delimiter (RFC 4180 2.2), so
+  //       // we explicitly check if we should trim off a row delimiter.
+  //       if (
+  //         this._data[nextIndex - 1] ===
+  //         this._rowDelimiter[this._rowDelimiter.length - 1]
+  //       ) {
+  //         trimRight += this._rowDelimiter.length;
+  //       }
+  //     }
+  //   } else {
+  //     // The next field starts at the next column offset.
+  //     nextIndex = this._getOffsetIndex(row, column + 1);
 
-      // Trim off the delimiter if it exists at the end of the field
-      if (index < nextIndex && this._data[nextIndex - 1] === this._delimiter) {
-        trimRight += 1;
-      }
-    }
+  //     // Trim off the delimiter if it exists at the end of the field
+  //     if (index < nextIndex && this._data[nextIndex - 1] === this._delimiter) {
+  //       trimRight += 1;
+  //     }
+  //   }
 
-    // Check to see if the field begins with a quote. If it does, trim a quote on either side.
-    if (this._data[index] === this._quote) {
-      trimLeft += 1;
-      trimRight += 1;
-    }
-    this._data = (this._data.slice(0, index + trimLeft)
-      + value
-      + this._data.slice(nextIndex - trimRight, this._data.length)
-    )
-  }
+  //   // Check to see if the field begins with a quote. If it does, trim a quote on either side.
+  //   if (this._data[index] === this._quote) {
+  //     trimLeft += 1;
+  //     trimRight += 1;
+  //   }
+  //   this._data = (this._data.slice(0, index + trimLeft)
+  //     + value
+  //     + this._data.slice(nextIndex - trimRight, this._data.length)
+  //   )
+  //   this._parseAsync()
+  // }
 
   /**
    * Reset the parser state.
